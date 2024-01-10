8a6675005ead57a070040bbc329198f2b17a3c5a
mm <matthias-mehdi.kaddour@epita.fr>
2024-01-10 03:33:24 +0100

    wip: read entire input into a buffer

---
+++ src/input/input.c
@@ -0,0 +1,65 @@
+#include "input.h"
+
+char *input_error(char *input, FILE *fd)
+{
+	free(input);
+	if (fd)
+		fclose(fd);
+	fprintf(stderr, "The command line syntax is:");
+	fprintf(stderr, " 42sh [OPTIONS] [SCRIPT] [ARGUMENTS ...]\n");
+	return NULL;
+}
+
+char *get_input(int argc, char ** argv)
+{
+	if (argc > 3)
+		return input_error(NULL, NULL);
+	char *input = 0;
+	size_t input_size = 0;	
+	FILE *fd = fdopen(0, "r");
+	for (int i = 1; i < argc; i++)
+	{
+		if (! strcmp(argv[i], "-c"))
+		{
+			i++;
+			if (i == argc)
+				return input_error(NULL, fd);
+			fd = fmemopen(argv[i], strlen(argv[i]), "r");
+		}
+		else if (! strcmp(argv[i], "-e"))
+		{
+			if (i == argc)
+				return input_error(NULL, fd);
+			i++;
+			fd = fopen(argv[i], "r");
+		}
+		else if (! strcmp(argv[i], "--verbose"))
+		{
+			if (fd->_fileno != 0)
+				return input_error(NULL, fd);
+			setenv("VERBOSE", "1", 0);
+		}
+		else if (! strcmp(argv[i], "--pretty-print"))
+		{
+			if (fd->_fileno != 0)
+				return input_error(NULL, fd);
+			setenv("PRETTY_PRINT", "1", 0);
+		}
+		else
+			return input_error(NULL, fd);
+	}
+
+	char buffer[128];
+	size_t a = 1;
+
+	do
+	{
+		a = fread(buffer, 1, 128, fd);
+		input_size += a;
+		input = realloc(input, input_size * sizeof(char));
+		memcpy(input + input_size - a, buffer, a);
+	} while (a > 0);
+	
+	fclose(fd);
+	return input;
+}
---
+++ src/input/input.h
@@ -0,0 +1,17 @@
+#ifndef	INPUT_H
+#define INPUT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+enum input_status
+{
+    INPUT_OK,
+    INPUT_ERROR
+};
+
+char *input_error(char *input, FILE *fd);
+char *get_input(int argc, char **argv);
+
+#endif /* INPUT_H */
