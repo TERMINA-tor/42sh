26628fd2c9f43ee0e0ac1fd6f40907f31bc377f1
Marc WANG <marc.wang@epita.fr>
2024-01-11 00:35:29 +0100

    feat: test lexer

---
+++ tests/test_lexer.py
@@ -0,0 +1,40 @@
+import subprocess as sp
+
+def test_lexer_one_word():
+    output = sp.run(["./eval_token", "echo"], capture_output=True, text=True)
+    assert output.stdout == "word\nEOF\n"
+
+def test_lexer_two_word():
+    output = sp.run(["./eval_token", "echo abc"], capture_output=True, text=True)
+    print(output)
+    assert output.stdout == "word\nword\nEOF"
+
+def test_lexer_multiple_word():
+    output = sp.run(["./eval_token", "echo salut salut salut je sais pas salut"], capture_output=True, text=True)
+    print(output)
+    assert output == "word\nword\nword\nword\nword\nword\nword\nword\nEOF"
+
+def test_lexer_if_else():
+    output = sp.run(["./eval_token", "if true then else fi"], capture_output=True, text=True)
+    expected_output = "if\nword\nthen\nelse\nfi\nEOF\n"
+    assert output.stdout == expected_output
+
+def test_lexer_semicolon():
+    output = sp.run(["./eval_token", "command1; command2"], capture_output=True, text=True)
+    expected_output = "word\nsemicolon\nword\nEOF\n"
+    assert output.stdout == expected_output
+
+def test_lexer_quote():
+    output = sp.run(["./eval_token", "echo 'Hello, World!'"], capture_output=True, text=True)
+    expected_output = "word\nquote\nword\nquote\nEOF\n"
+    assert output.stdout == expected_output
+
+def test_lexer_elif():
+    output = sp.run(["./eval_token", "if true then elif true then else fi"], capture_output=True, text=True)
+    expected_output = "if\nword\nthen\nelif\nword\nthen\nelse\nfi\nEOF\n"
+    assert output.stdout == expected_output
+
+def test_lexer_apostrophe_inside_word():
+    output = sp.run(["./eval_token", "echo aa'aa'"], capture_output=True, text=True)
+    expected_output = "word\nquote\nword\nquote\nEOF\n"
+    assert output.stdout == expected_output
