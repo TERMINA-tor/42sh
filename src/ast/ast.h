d488ab38f18f45852e0b8fa518e91d2c3ffbbb79
Drogonito <drogonito@pop-os.localdomain>
2024-01-11 13:41:18 +0100

    little correction ast endif
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -1,30 +1,31 @@
 #ifndef AST_H
 #define AST_H
 
+#include <stddef.h>
+
 enum ast_type
 {
-	AST_IF,
-	AST_ELIF,
-	AST_WORD,
-	AST_COMMAND
+    AST_IF,
+    AST_ELIF,
+    AST_WORD,
+    AST_COMMAND
 };
 
 struct ast
 {
-	enum ast_type type; ///< node type
-	struct ast **children; ///< list of children
-	size_t nbchildren; ///< number of children
+    enum ast_type type; ///< node type
+    struct ast **children; ///< list of children
+    size_t nbchildren; ///< number of children
 };
-
 
 /**
  ** \brief Allocates a node with the given type
  */
 struct ast *ast_new(enum ast_type type);
 
-
 /**
  ** \brief free the given ast
  */
 
 void ast_free(struct ast *ast);
+#endif
