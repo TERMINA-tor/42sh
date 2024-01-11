2d479b9503add599ebb1a6d6dfdf765a9a95cc34
Drogonito <drogonito@pop-os.localdomain>
2024-01-11 14:14:11 +0100

    fix: forgot to delete repo after name change
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

--- src/pretty_print/little_print_ast.h
+++
@@ -1,9 +0,0 @@
-#ifndef LITTLE_PRINT_AST_H
-#define LITTLE_PRINT_AST_H
-
-#include "../ast/ast.h"
-
-// Function prototype for little-pretty-printing an AST
-void little_print_ast(struct ast *res, int depth);
-
-#endif // LITTLE_PRINT_AST_H
