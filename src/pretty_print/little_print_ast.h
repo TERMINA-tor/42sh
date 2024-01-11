79e7c0c5573021478e9b57be34a21c630952586b
Drogonito <drogonito@pop-os.localdomain>
2024-01-11 13:55:59 +0100

    feat: add little_print_ast
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

---
+++ src/pretty_print/little_print_ast.h
@@ -0,0 +1,9 @@
+#ifndef LITTLE_PRINT_AST_H
+#define LITTLE_PRINT_AST_H
+
+#include "../ast/ast.h"
+
+// Function prototype for little-pretty-printing an AST
+void little_print_ast(struct ast *res, int depth);
+
+#endif // LITTLE_PRINT_AST_H
