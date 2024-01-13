e0a65983e3e1aac1a447503b4fdf161aff9104a3
Drogonito <drogonito@pop-os.localdomain>
2024-01-13 17:15:50 +0100

    fix: clang-tidy for (void) type
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

--- src/eval/builtins.h
+++ src/eval/builtins.h
@@ -2,10 +2,10 @@
 #define BUILTINS_H
 
 // The 'true' built-in function
-int builtin_true();
+int builtin_true(void);
 
 // The 'false' built-in function
-int builtin_false();
+int builtin_false(void);
 
 // The 'echo' built-in function
 int builtin_echo(char **argv);
