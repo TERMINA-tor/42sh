47944675df2b0f6fb122c4d4822ce5fd91394f10
Marc WANG <marc.wang@epita.fr>
2024-01-24 20:56:39 +0100

    feat: parser rule shell

---
+++ src/parser/rule_shell.c
@@ -0,0 +1,57 @@
+#include "rule_shell.h"
+
+enum parser_status parse_shell_command(struct ast **ast, struct lexer *lexer){
+    return parse_if(ast, lexer);
+}
+
+enum parser_status parse_if(struct ast **ast, struct lexer *lexer){
+    if (lexer_peek(lexer).type != TOKEN_IF) // must begin with TOKEN_IF
+        return PARSER_UNEXPECTED_TOKEN;
+    lexer_pop(lexer);
+    struct ast *node = (struct ast *)ast_if_init();
+    if (parse_compound_list(&(((struct ast_if*)node)->condition), lexer) != PARSER_OK) // check compound list
+        return PARSER_UNEXPECTED_TOKEN;
+    if (lexer_peek(lexer).type != TOKEN_THEN) // check then token
+        return PARSER_UNEXPECTED_TOKEN;
+    lexer_pop(lexer);
+    if (parse_compound_list(&(((struct ast_if*)node)->then_body), lexer) != PARSER_OK) // check compound list
+        return PARSER_UNEXPECTED_TOKEN;
+    if (lexer_peek(lexer).type == TOKEN_ELSE) // check else token
+        parse_else_clause(&(((struct ast_if*)node)->else_body), lexer);
+
+    if (lexer_peek(lexer).type != TOKEN_FI) // check fi token
+        return PARSER_UNEXPECTED_TOKEN;
+    lexer_pop(lexer);
+    return PARSER_OK; 
+}
+
+enum parser_status parse_else_clause(struct ast **ast, struct lexer *lexer){
+    if (lexer_peek(lexer).type == TOKEN_ELSE) // check token else
+    {
+        lexer_pop(lexer);
+
+        if (parse_compound_list(ast, lexer) != PARSER_OK) // check compound list
+            return PARSER_UNEXPECTED_TOKEN;
+        return PARSER_OK;
+    }
+    else if (lexer_peek(lexer).type == TOKEN_ELIF){ // check elif token
+        lexer_pop(lexer);
+        *ast = (struct ast *)ast_if_init();
+
+        if (parse_compound_list(&(((struct ast_if*)ast)->condition), lexer) != PARSER_OK) // check compound list
+            return PARSER_UNEXPECTED_TOKEN;
+
+        if (lexer_peek(lexer).type != TOKEN_THEN) // check then token
+            return PARSER_UNEXPECTED_TOKEN;
+        lexer_pop(lexer);
+
+        if (parse_compound_list(&(((struct ast_if*)ast)->then_body), lexer) != PARSER_OK) // check compound list
+            return PARSER_UNEXPECTED_TOKEN;
+
+        if (parse_else_clause(&(((struct ast_if*)ast)->else_body), lexer) != PARSER_OK) // check else_clause
+            return PARSER_UNEXPECTED_TOKEN;
+        return PARSER_OK;
+    }
+    else
+        return PARSER_UNEXPECTED_TOKEN;
+}
