60f0d8c24727314758fd26a3d8ad156f0ca64374
Drogonito <drogonito@pop-os.localdomain>
2024-01-25 18:50:11 +0100

    fix:correction of eval with new ast
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

--- src/eval/evaluation_process.c
+++ src/eval/evaluation_process.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "../ast/ast.h"
 #include "builtins.h"
-#include "stdio.h"
-#include "string.h"
 
-int execute_command(struct ast *command_node);
+int execute_command(struct ast_cmd *command_node);
 char **convert_children_to_argv(struct ast *node);
-int evaluate_if(struct ast *if_node);
+int evaluate_if(struct ast_if *if_node);
 
 int evaluate_node(struct ast *node)
 {
@@ -17,28 +18,25 @@ int evaluate_node(struct ast *node)
     switch (node->type)
     {
     case AST_IF:
-        return evaluate_if(node);
+        return evaluate_if((struct ast_if *)node);
     case AST_COMMAND:
-        return execute_command(node);
-    case AST_WORD:
-        fprintf(stderr, "Unexpected word: %s\n", node->value);
-        return -1;
+        return execute_command((struct ast_cmd *)node);
     default:
         fprintf(stderr, "Unknown AST node type\n");
         return -1;
     }
 }
 
-int evaluate_if(struct ast *if_node)
+int evaluate_if(struct ast_if *if_node)
 {
-    if (if_node == NULL || if_node->nbchildren == 0)
+    if (if_node->condition == NULL || if_node->then_body == NULL)
     {
         fprintf(stderr, "Invalid if statement\n");
         return -1;
     }
 
     // Evaluate the condition
-    int condition_result = evaluate_node(if_node->children[0]);
+    int condition_result = evaluate_node(if_node->condition);
     if (condition_result == -1)
     {
         fprintf(stderr, "Error evaluating if condition\n");
@@ -48,62 +46,57 @@ int evaluate_if(struct ast *if_node)
     if (condition_result == builtin_true())
     {
         // If condition is true and there is a 'true' block
-        if (if_node->nbchildren > 1)
-        {
-            return evaluate_node(if_node->children[1]);
-        }
+        return evaluate_node(if_node->then_body);
     }
     else
     {
         // If condition is false and there is an 'else' block
-        if (if_node->nbchildren > 2)
+        if (if_node->else_body != NULL)
         {
-            return evaluate_node(if_node->children[2]);
+            return evaluate_node(if_node->else_body);
         }
     }
 
     return 0;
 }
 
-int execute_command(struct ast *command_node)
+int execute_command(struct ast_cmd *command_node)
 {
-    if (strcmp(command_node->value, "echo") == 0)
+    if (strcmp(*command_node->words, "echo") == 0)
     {
-        char **argv = convert_children_to_argv(command_node);
-        int res = builtin_echo(argv);
-        free(argv);
+        int res = builtin_echo(command_node->words + 1);
         return res;
     }
-    else if (strcmp(command_node->value, "true") == 0)
+    else if (strcmp(*command_node->words, "true") == 0)
     {
         return builtin_true();
     }
-    else if (strcmp(command_node->value, "false") == 0)
+    else if (strcmp(*command_node->words, "false") == 0)
     {
         return builtin_false();
     }
     else
     {
-        fprintf(stderr, "Unknown command: %s\n", command_node->value);
+        fprintf(stderr, "Unknown command: %s\n", *command_node->words);
         return -1;
     }
 }
 
-char **convert_children_to_argv(struct ast *node)
-{
-    char **argv = malloc((node->nbchildren + 2) * sizeof(char *));
-    if (!argv)
-    {
-        perror("malloc failed");
-        exit(EXIT_FAILURE);
-    }
-
-    argv[0] = node->value; // Command name
-    for (size_t i = 0; i < node->nbchildren; i++)
-    {
-        argv[i + 1] = node->children[i]->value;
-    }
-    argv[node->nbchildren + 1] = NULL; // Null-terminate the argv array
-
-    return argv;
-}
+// char **convert_children_to_argv(struct ast *node)
+// {
+//     char **argv = malloc((node->nbchildren + 2) * sizeof(char *));
+//     if (!argv)
+//     {
+//         perror("malloc failed");
+//         exit(EXIT_FAILURE);
+//     }
+//
+//     argv[0] = node->value; // Command name
+//     for (size_t i = 0; i < node->nbchildren; i++)
+//     {
+//         argv[i + 1] = node->children[i]->value;
+//     }
+//     argv[node->nbchildren + 1] = NULL; // Null-terminate the argv array
+//
+//     return argv;
+// }
