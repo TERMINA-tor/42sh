679a22b9b8e8c3979a057fff4e93338b0aa08306
Drogonito <drogonito@pop-os.localdomain>
2024-01-31 16:59:05 +0100

    fix: clang format
    Signed-off-by: Drogonito <drogonito@pop-os.localdomain>

--- src/eval/eval_redirections.h
+++ src/eval/eval_redirections.h
@@ -1,10 +1,6 @@
 #ifndef EVAL_REDIRECTIONS_H
 #define EVAL_REDIRECTIONS_H
 
-#include "../ast/ast.h"
-#include "../lexer/lexer.h"
-#include "evaluation_process.h"
-
 #include <fcntl.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -12,6 +8,10 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#include "../ast/ast.h"
+#include "../lexer/lexer.h"
+#include "evaluation_process.h"
 
 int eval_redirections(struct ast *node);
 
